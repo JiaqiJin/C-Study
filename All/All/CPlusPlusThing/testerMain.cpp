7f17a743d6c6050a54412754d1adbee6b1fca4ff
JiaqiJin96 <fdsam@hotmail.com>
2020-08-07 13:50:35 +0200

    exmaple update

--- All/All/CPlusPlusThing/testerMain.cpp
+++ All/All/CPlusPlusThing/testerMain.cpp
@@ -21,6 +21,27 @@ void error_mesg(std::initializer_list<string> li)
 	}
 }
 
+void test3()
+{
+	int* p = (int *)malloc(sizeof(int) * 10);
+	*p = 10;
+	cout << *p << endl;
+	//000001 000001 000001 000001
+	memset(p, 0, sizeof(int));
+	cout << *p;
+}
+
+class Person {
+public:
+	int id;
+	int age;
+	int height;
+	Person(int id, int age, int height) 
+		:id(id),age(age),height(height){}
+};
+
+
+
 int main()
 {
 	int i = 42;
@@ -41,16 +62,23 @@ int main()
 
 	//cout << *p << "  " << sizeof(p);
 
-	char* p1 = (char*)malloc(2);
+	/*char* p1 = (char*)malloc(2);
 	p1[0] = 1;
 	p1[1] = 10;
 	cout << p1[0] << " " << p1[1] << endl;
 	*p1 = 10;
 	*(p1 + 1) = 1000;
 	cout << p1[0] << " " << p1[1] << endl;
+	*/
 
+	//test3();
 
-	free(p1);
+	Person person(10, 10, 10);
+	std::cout << person.id << " " << person.age << "  " << person.height << endl;
+
+	memset(&person, NULL, sizeof(Person));
+
+	std::cout << person.id << " " << person.age << "  " << person.height << endl;
 
 	return 0;
 }
